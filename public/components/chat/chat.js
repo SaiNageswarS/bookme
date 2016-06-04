e27410f3b8846f31dd987aa8655de1eebd9a04f2
SaiNageswarS <sainageswar@gmail.com>
2016-06-05 01:28:25 +0530

    refractoring for firebase

---
+++ public/components/chat/chat.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('bookme')
+
+.directive('bmChat', [function() {
+    return {
+        scope: {},
+        controller: function() {
+            
+        },
+        templateUrl: 'components/chat/chat.html'
+    }
+
+}]);
