809f5ef16bca64058ccd71e3aa90fe95014f9ec9
SaiNageswarS <sainageswar@gmail.com>
2016-06-04 22:22:37 +0530

    initial commit

---
+++ components/header/header.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('bookme')
+
+.directive('bmHeader', [function() {
+    return {
+        scope: {},
+        controller: function() {
+            
+        },
+        templateUrl: 'components/header/header.html'
+    }
+
+}]);
