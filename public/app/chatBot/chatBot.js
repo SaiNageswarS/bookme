6b34402a1d5a3effdedbbc296605580abcda649c
SaiNageswarS <sainageswar@gmail.com>
2016-06-06 00:13:42 +0530

    receiving messages

--- public/app/chatBot/chatBot.js
+++ public/app/chatBot/chatBot.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Todo: remove it as constant
+var appId = "a48475b7-7eb3-4b02-841e-280d50c1a0da";
+
 angular.module('bookme')
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,11 +12,25 @@ angular.module('bookme')
   });
 }])
 
-.controller('chatBotCtrl', function($scope, UserService) {  
+.controller('chatBotCtrl', function($scope, UserService, ChatService) {  
     UserService.login()
       .then(function(currentUser) {
         console.log(currentUser);
         $scope.currentUser = currentUser;
+        
+        var chatInstance = ChatService.getInstance($scope.currentUser.uid, appId);
+        
+        $scope.sendMessage = function() {
+          var text = document.getElementById("chatTextContent").textContent;
+          chatInstance.sendMessage(text);
+          document.getElementById("chatTextContent").innerHTML = "";
+        };
+        
+        $scope.messages = [];
+        chatInstance.receiveMessages(function(chat) {
+            $scope.messages.push(chat);
+            $scope.$apply();
+        });
       })
       .catch(function(err) {
         console.log(err);
--- public/services/chatService.js
+++ public/services/chatService.js
@@ -1,4 +1,42 @@
 angular.module('bookme')
 .factory('ChatService', function() {
+    var rootRef = firebase.database();
     
+    var FirebaseChatInstance = function (userId, appId) {
+        var self = this;
+        self.userId = userId;
+        self.appId = appId;
+        
+        var chatRef = rootRef.ref('messages/' + self.appId + "/" + self.userId);
+        
+        self.sendMessage = function (text) {
+            if (!text || text.length === 0) {
+                return;
+            }
+            var message = {
+                sender: self.userId,
+                sentOn: (new Date()).getTime(),
+                type: 'text',
+                content: text
+            };
+            
+            var newMessageRef = chatRef.push();
+            newMessageRef.set(message);
+        };
+        
+        self.receiveMessages = function(cb) {
+          chatRef.orderByChild("sentOn").limitToLast(20)
+            .on("child_added", function(snapshot) {
+                    var chat = snapshot.val();
+                    cb(chat);       
+                });  
+        };
+        
+    };
+    
+    return {
+        getInstance: function(userId, appId) {
+            return new FirebaseChatInstance(userId, appId);
+        }
+    };
 });
